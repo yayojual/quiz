7e1d26ec2d30c3987a86ed5dff13591cd353ad17
juanma <juanmanuel.vaca@cenits.es>
2015-07-29 22:01:06 +0200

    Carga de más preguntas en BD

--- models/models.js
+++ models/models.js
@@ -40,6 +40,12 @@ sequelize.sync().then(function() {
 		if(count === 0){ //la tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 				      respuesta: 'Roma'
+				   });
+			Quiz.create({ pregunta: 'Capital de Portugal',
+				      respuesta: 'Lisboa'
+				   });
+			Quiz.create({ pregunta: 'Capital de Francia',
+				      respuesta: 'París'
 				   })
 			.then(function(){console.log('Base de datos inicializada')});
 		};

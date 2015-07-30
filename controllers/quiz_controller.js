32569e52b6b4e4e28baec3ada8ebaf2d8bd07bbd
juanma <juanmanuel.vaca@cenits.es>
2015-07-30 20:52:39 +0200

    añadido buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,9 +12,19 @@ exports.load = function(req, res, next, quizId){
 
 //GET /quizes
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes) {
-	res.render('quizes/index', {quizes: quizes});
-	}).catch(function(error){next(error);});
+	if (req.query.text){
+		var text = '%';
+		text += (req.query.text).replace(/ /g,'%');
+		text += '%';
+		models.Quiz.findAll({where: ["pregunta like ?", text], order:"pregunta"}).then(function(quizes) {  
+		res.render('quizes/index', {quizes: quizes});
+		}).catch(function(error){next(error);});
+	}
+	else{
+		models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index', {quizes: quizes});
+		}).catch(function(error){next(error);});
+	}
 };
 
 //GET /quizes/:id

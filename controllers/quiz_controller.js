86f28229e85698b0552822755a71704e58773732
juanma <juanmanuel.vaca@cenits.es>
2015-07-31 10:27:31 +0200

    añadido borrado preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,7 +44,7 @@ exports.answer = function(req, res){
 //GET /quizes/new
 exports.new = function(req, res){
 	var quiz = models.Quiz.build(  //crea objeto quiz
-		{pregunta: "Pregunta", Respuesta: "Respuesta"}
+		{pregunta: "", respuesta: ""}
 	);
 	res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -84,4 +84,11 @@ exports.update = function(req, res){
 			})
 		}
 	});	
+};
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res){
+	req.quiz.destroy().then(function() {
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
 };
